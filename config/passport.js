6e4afcbaf9b07397917bc651fc89ae6fe615414c
Ruby <r844312@gmail.com>
2020-05-24 23:56:34 +0800

    Add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
@@ -7,8 +8,14 @@ module.exports = passport => {
     passport.use(new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
         User.findOne( { email: email }).then(user => {
             if (!user) { return done(null, false), { message: 'That email is not registered'}}
-            if (user.password != password) { return done(null, false), { message: 'Email or Password is incorrect'}}
-            return done(null, user)
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err
+                if (isMatch) {
+                    return done(null, user)
+                } else {
+                    return done(null, false), { message: 'Email or Password is incorrect'}
+                }
+            })
         })
     }))
     passport.serializeUser((user, done) => {
